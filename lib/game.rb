232b29608ccb28043b3e47a70b4b83fe6704610f
Clinton Herrick & Trevor Scherling <student@epicodus.com>
2015-09-23 14:59:17 -0700

    determin which of the players will win, lose or if the game is a tie

--- lib/game.rb
+++ lib/game.rb
@@ -5,10 +5,22 @@ def game(player_1, player_2)
   case
     when player_1 == "rock" && player_2 == "scissor"
       outcome="player 1 wins"
+    when player_1 == "paper" && player_2 == "rock"
+      outcome="player 1 wins"
+    when player_1 == "scissor" && player_2 == "paper"
+      outcome = "player 1 wins"
     when player_1 == "rock" && player_2 == "paper"
-      outcome="player 2 wins"
+      outcome = "player 2 wins"
+    when player_1 == "scissor" && player_2 == "rock"
+      outcome = "player 2 wins"
+    when player_1 == "paper" && player_2 == "scissor"
+      outcome = "player 2 wins"
+    when player_1 == "paper" && player_2 == "paper"
+      outcome = "tie"
+    when player_1 == "scissor" && player_2 == "scissor"
+      outcome = "tie"
     when player_1 == "rock" && player_2 == "rock"
-       outcome= "tie"  
+      outcome= "tie"
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,16 +8,54 @@ describe('game') do
     expect(game(player_1, player_2)).to(eq("player 1 wins"))
   end
 
+  it("compairs 'paper' to 'rock' returing 'paper' as the winner") do
+    player_1= "paper"
+    player_2= "rock"
+    expect(game(player_1, player_2)).to(eq("player 1 wins"))
+  end
+
+  it("compairs 'scissor' to 'paper' returning 'paper' as the winner") do
+    player_1 = "scissor"
+    player_2 = "paper"
+    expect(game(player_1, player_2)).to(eq("player 1 wins"))
+  end
+
   it("compairs 'rock' to 'paper' returing 'paper' as the winner") do
-    player_1= "rock"
-    player_2= "paper"
+    player_1 = "rock"
+    player_2 = "paper"
     expect(game(player_1, player_2)).to(eq("player 2 wins"))
   end
+
+  it("compairs 'scissor' to 'rock' return 'rock' as the winner") do
+    player_1 = "scissor"
+    player_2 = "rock"
+    expect(game(player_1, player_2)).to(eq("player 2 wins"))
+  end
+
+  it("compairs 'paper' to 'scissor' return 'scissor' as the winner") do
+    player_1 = "paper"
+    player_2 = "scissor"
+    expect(game(player_1, player_2)).to(eq("player 2 wins"))
+  end
+
+  it ("compairs 'paper' to 'paper' returns 'tie'") do
+    player_1 = "paper"
+    player_2 = "paper"
+    expect(game(player_1, player_2)).to(eq("tie"))
+  end
+
+  it("compairs 'scissor' to 'scissor' and returns 'tie'")do
+  player_1 = "scissor"
+  player_2 = "scissor"
+  expect(game(player_1, player_2)).to(eq("tie"))
+end
 
   it("compairs 'rock' to 'rock' returing as a tie") do
     player_1 = "rock"
     player_2 = "rock"
     expect(game(player_1, player_2)).to(eq("tie"))
   end
+
+
 
 end
